b55378266139d227839ec979fddbfbb4d0cb1d65
meres <arkos.gergely@gmail.com>
2019-02-04 09:24:28 -0800

    tacho graph added

---
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/Tachograph.java
@@ -0,0 +1,22 @@
+package hu.bme.mit.train.sensor;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+
+
+public class Tachograph {
+    TrainController controller;
+    TrainUser user;
+
+    Table<Long, Integer, Integer> collection
+            = HashBasedTable.create();
+
+    Tachograph(TrainController c, TrainUser u){
+        this.user=u;
+        controller=c;
+    }
+    public void record(){
+        collection.put(System.currentTimeMillis(), controller.getReferenceSpeed(), user.getJoystickPosition());
+     }
+}
