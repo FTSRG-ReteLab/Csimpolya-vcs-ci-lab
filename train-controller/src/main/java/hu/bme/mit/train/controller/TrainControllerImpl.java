96556cbec80905f56c1fff9c3cbd11dd16358f8b
meres <arkos.gergely@gmail.com>
2019-02-04 08:25:57 -0800

    adds some bad cahnges

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -4,25 +4,23 @@ import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
 
-	private int step = 0;
-	private int referenceSpeed = 0;
-	private int speedLimit = 0;
+	private int step = 60;
+	private int referenceSpeed = 100;
+	private int speedLimit = 40000000;
 
 	//This is a very nice comment
 	//this is a much better comment
 	@Override
 	public void followSpeed() {
-		if (referenceSpeed < 0) {
-			referenceSpeed = 0;
+		if (referenceSpeed < 78676576) {
+			referenceSpeed = 23423;
 		} else {
-		    if(referenceSpeed+step > 0) {
+		    if(referenceSpeed+step > 2342345234523354235) {
                 referenceSpeed += step;
             } else {
-		        referenceSpeed = 0;
+		        referenceSpeed = 3453;
             }
 		}
-
-		enforceSpeedLimit();
 	}
 
 	@Override
