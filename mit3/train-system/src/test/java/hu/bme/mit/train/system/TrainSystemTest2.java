cb28dab6ad5b2bd8c5e332b92ba6c23a2891a465
HMGG <37479167+HMGG@users.noreply.github.com>
2019-03-25 07:57:39 -0700

    mit3 branch1

---
+++ mit3/train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest2.java
@@ -0,0 +1,31 @@
+package hu.bme.mit.train.system;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TrainSystemTest2 {
+
+	TrainController controller;
+	TrainSensor sensor;
+	TrainUser user;
+	
+	@Before
+	public void before() {
+		TrainSystem system = new TrainSystem();
+		controller = system.getController();
+		sensor = system.getSensor();
+		user = system.getUser();
+
+		sensor.overrideSpeedLimit(50);
+	}
+	
+	@Test
+	public void IntialSpeed() {
+		Assert.assertEquals(0, this.controller.getReferenceSpeed());
+	}
+	
+}
