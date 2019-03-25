fe04caef12dc38cb4db8895c0c5055f5e628d465
arkosg <arkos.gergely@gmail.com>
2019-03-25 16:45:07 +0100

    This is my change (ÁG)

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -7,16 +7,20 @@ import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
 
-public class TrainSystem {
+public class TrainSystem implements Runnable{
 
 	private TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
+	
+	public TrainSystem() {
+		new Thread(this).start();
+	}
 
 	public TrainController getController() {
 		return controller;
 	}
-
+	
 	public TrainSensor getSensor() {
 		return sensor;
 	}
@@ -25,4 +29,15 @@ public class TrainSystem {
 		return user;
 	}
 
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				this.wait(500);
+				this.controller.followSpeed();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
